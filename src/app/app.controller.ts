5af0ec708e5288297455b77ddeeb348496da39e4
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-05-01 15:40:55 +0800

    updated healtcheck endpoint

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -2,7 +2,10 @@ import { Controller, Get, HttpStatus, Res } from "@nestjs/common";
 import { Response } from "express";
 import { AppService } from "./app.service";
 
-@Controller()
+@Controller({
+  path: "system",
+  version: "1",
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
